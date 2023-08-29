0ad82c06644a4eee017f7e90a3b488799870f635
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-29 23:32:52 +0530

    mongoose connect

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require("express")
+require("dotenv").config()
+const multer = require("multer")
+const mongoose = require("mongoose")
 
+const express = require("express")
 const app = express()
+
+const upload = multer({dest:  "uploads"})
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -8,8 +15,8 @@ app.get("/", (req,res) => {
     res.render("index")
 })
 
-app.post("/upload", (req,res) => {
+app.post("/upload", upload.single("file"), (req,res) => {
     res.send("hi")
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
