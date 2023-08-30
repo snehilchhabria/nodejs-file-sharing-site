98e185eafc443320f1bb72fcb71e036038069ff8
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-08-30 21:03:04 +0530

    file sharing logic

--- server.js
+++ server.js
@@ -27,8 +27,19 @@ app.post("/upload", upload.single("file"), async (req,res) => {
     }
 
     const file = await File.create(fileData)
-    console.log(file)
-    res.send(file.originalName)
+    
+    res.render("index", { fileLink : `${req.headers.origin}/file/${file.id}`})
+})
+
+app.get("/file/:id", async (req,res) => {
+    res.send(req.params.id)
+    const file = await File.findById(req.params.id)
+
+    file.downloadCount++
+    await file.save()
+    console.log(file.downloadCount)
+
+    res.download(file.path, file.originalName)
 })
 
 app.listen(process.env.PORT)
